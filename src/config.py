b51dcbe7951b78cb009907b8bd91d3e1b2f0b67f
Ray Tian <3093391046@qq.com>
2024-10-27 18:03:02 +0800

    优化了初次运行的逻辑

--- src/config.py
+++ src/config.py
@@ -1,9 +1,14 @@
 import json
 import os
+from tkinter import messagebox
+
 
 def read_config():
     if not os.path.exists("./conf/config.json"):
-        return create_default_config()
+        messagebox.showwarning("警告", "配置文件不存在，已创建默认配置文件。请到目录下的 conf/config.json 中配置你的账号密码。"
+                                      "之后请重新运行本程序。")
+        create_default_config()
+        exit(0)
     with open('./conf/config.json', 'r', encoding='utf-8') as f:
         config = json.load(f)
     return config
